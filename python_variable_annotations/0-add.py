4eed35ba07a4075207f50500b70e71d524030d40
ReemaAlshahrani <reema.alshahrani.ali@gmail.com>
2026-08-25 13:19:37 +0000

    feat(variable_annotations): add float addition function

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module for adding two floating-point numbers."""
+
+
+def add(a: float, b: float) -> float:
+  """Add two float numbers and return their sum as a float."""
+  return a + b
