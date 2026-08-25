b15a43cc42579406e46563af159ce8e6e458f69c
ReemaAlshahrani <reema.alshahrani.ali@gmail.com>
2026-08-25 13:45:21 +0000

    feat(variable_annotations): add floor function returning int

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module that returns the floor of a float number."""
+
+import math
+
+
+def floor(n: float) -> int:
+    """Return the floor of the float n."""
+    return math.floor(n)
