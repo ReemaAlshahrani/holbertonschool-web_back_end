6736d4b1670fd7d97aca2adcfc54b9eb239f0219
ReemaAlshahrani <reema.alshahrani.ali@gmail.com>
2026-08-25 15:35:53 +0000

    feat(variable_annotations): add to_kv function returning a tuple

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -5,5 +5,7 @@ from typing import Tuple, Union
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    """Return a tuple containing the string k and the square of v as a float."""
+    """Return a tuple containing the string k 
+    and the square of v as a float.
+    """
     return (k, v * v)
