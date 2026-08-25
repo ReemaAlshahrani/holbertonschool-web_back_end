855702821d3239f92a240c8567bd3641e21558b0
ReemaAlshahrani <reema.alshahrani.ali@gmail.com>
2026-08-25 13:55:34 +0000

    feat(variable_annotations): add to_str function converting float to string

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module that returns the string representation of a float number."""
+
+
+def to_str(n: float) -> str:
+    """Return the string representation of the float n."""
+    return str(n)
