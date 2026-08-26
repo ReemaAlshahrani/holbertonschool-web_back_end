5225dfde1a0b9a3b43e934b8206769db601787f6
ReemaAlshahrani <reema.alshahrani.ali@gmail.com>
2026-08-26 13:59:29 +0000

    feat(variable_annotations): annotate safe_first_element with Sequence and Optional types

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from typing import Sequence, Any, Optional
 
+
 # The types of the elements of the input are not known
 def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
     """Return the first element of a sequence safely."""
