4e1c5b8cfa2e6947336a063a1525a877e8ecac08
ReemaAlshahrani <reema.alshahrani.ali@gmail.com>
2026-08-26 13:40:43 +0000

    feat(variable_annotations): add element_length function with complex type annotations

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-"""Module that calculates the length of elements in an iterable using type annotations."""
+"""Module with element_length function."""
 
 from typing import Iterable, Sequence, List, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
-    """Return a list of tuples containing the sequence and its length."""
+    """Return list of elements and their lengths."""
     return [(i, len(i)) for i in lst]
