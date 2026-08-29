75783a2f5e1effa65134e84a00ddbc551b36b753
ReemaAlshahrani <reema.alshahrani.ali@gmail.com>
2026-08-29 22:06:01 +0200

    feat(Python - Async Comprehension): fix async_generator requirements and implementation details

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Module for asynchronous generator coroutine."""
+"""Asynchronous generator module that yields 10 random numbers."""
 
 import asyncio
 import random
@@ -7,7 +7,7 @@ from typing import AsyncGenerator
 
 
 async def async_generator() -> AsyncGenerator[float, None]:
-    """Loop 10 times, asynchronously wait 1 second, and yield a random number."""
+    """Loop 10 times, wait 1 second, and yield a random float between 0 and 10."""
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
