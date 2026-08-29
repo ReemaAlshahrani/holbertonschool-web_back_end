e0f15535576d4c1a9fadc80cd07132acd38f59f3
ReemaAlshahrani <reema.alshahrani.ali@gmail.com>
2026-08-29 22:51:23 +0200

    feat(Python - Async Comprehension): finalize async_generator implementation

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
-"""Module that defines an asynchronous generator coroutine."""
-
+"""Module with an async generator that
+yields random floats after a delay."""
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
-    """Loop 10 times, asynchronously wait 1 second, and yield a random float."""
+async def async_generator() -> Generator[float, None, None]:
+    """Loop 10 times, sleeping 1 second before yielding a
+     random float each iteration."""
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
