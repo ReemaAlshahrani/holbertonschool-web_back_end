d64f6ba64ac5f74702a983fbd0f4219b630c6ada
ReemaAlshahrani <reema.alshahrani.ali@gmail.com>
2026-08-29 22:09:03 +0200

    feat(Python - Async Comprehension): fix type annotations and docstring in async_generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,5 +1,6 @@
+#!/usr/init/env python3
 #!/usr/bin/env python3
-"""Asynchronous generator module that yields 10 random numbers."""
+"""Asynchronous generator that yields 10 random numbers."""
 
 import asyncio
 import random
@@ -7,7 +8,7 @@ from typing import AsyncGenerator
 
 
 async def async_generator() -> AsyncGenerator[float, None]:
-    """Loop 10 times, wait 1 second, and yield a random float between 0 and 10."""
+    """Loop 10 times, wait 1 second, and yield a random number."""
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
