e149a79322fa90beccd47687847e3853a94b8996
ReemaAlshahrani <reema.alshahrani.ali@gmail.com>
2026-08-29 22:20:16 +0200

    feat(Python - Async Comprehension): adjust yield and sleep order in async_generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -11,9 +11,9 @@ from typing import AsyncGenerator
 
 async def async_generator() -> AsyncGenerator[float, None]:
     """
-    A coroutine that loops 10 times, asynchronously waits 1 second
-    at each iteration, and yields a random float between 0 and 10.
+    A coroutine that loops 10 times, yields a random float
+    between 0 and 10, and asynchronously waits 1 second.
     """
     for _ in range(10):
-        await asyncio.sleep(1)
         yield random.uniform(0, 10)
+        await asyncio.sleep(1)
