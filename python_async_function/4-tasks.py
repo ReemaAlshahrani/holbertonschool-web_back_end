e9008f7236739e7c647c35aee54a0d6523275502
ReemaAlshahrani <reema.alshahrani.ali@gmail.com>
2026-08-29 21:48:58 +0200

    fix(Python - Async): update docstrings and formatting for task_wait_n to match requirements

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Module documentation: A module for running multiple tasks concurrently using task_wait_random.
+Module for running multiple tasks concurrently using task_wait_random.
 """
 
 import asyncio
@@ -10,8 +10,8 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """Spawns task_wait_random n times with the specified max_delay
-
+    """
+    Spawns task_wait_random n times with the specified max_delay
     and returns the list of all the delays in ascending order.
     """
     tasks = [task_wait_random(max_delay) for _ in range(n)]
