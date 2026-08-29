5e26671049c1de7f270f85f2374c9b560140b9d5
ReemaAlshahrani <reema.alshahrani.ali@gmail.com>
2026-08-29 22:37:51 +0200

    feat(Python - Async Comprehension): finalize async_generator implementation

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -7,7 +7,7 @@ from typing import AsyncGenerator
 
 
 async def async_generator() -> AsyncGenerator[float, None]:
-    """Loop 10 times, wait 1 second, and yield a random float."""
+    """Loop 10 times, asynchronously wait 1 second, and yield a random float."""
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
