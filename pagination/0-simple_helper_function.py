4686d1666e987a302b74904f9643a7d7914e6127
ReemaAlshahrani <reema.alshahrani.ali@gmail.com>
2026-09-06 16:32:55 +0000

    feat(pagination): update index range implementation

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -3,13 +3,9 @@
 Module for calculating pagination index range.
 """
 
+
 def index_range(page: int, page_size: int) -> tuple[int, int]:
     """Return a tuple of size two containing a start index and an end index."""
-    
-    # Calculate the start index for the pagination parameters
     start_index = (page - 1) * page_size
-    
-    # Calculate the end index for the pagination parameters
-    end_index = page_size * page
-    
+    end_index = page * page_size
     return (start_index, end_index)
