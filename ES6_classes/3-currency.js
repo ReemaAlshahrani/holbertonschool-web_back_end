5dc67f080a003b625fa82161258b73082040716d
ReemaAlshahrani <reema.alshahrani.ali@gmail.com>
2026-09-19 11:20:05 +0000

    feat(ES6 classes): Implement Currency class with getters, setters, and displayFullCurrency method

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,36 @@
+export default class Currency {
+  constructor(code, name) {
+    // Verify and assign attributes using setters
+    this.code = code;
+    this.name = name;
+  }
+
+  // Getter and Setter for code
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._code = value;
+  }
+
+  // Getter and Setter for name
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = value;
+  }
+
+  // Method to display full currency format: name (code)
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
