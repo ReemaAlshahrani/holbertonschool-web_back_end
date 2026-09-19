442713637abb21e0f7ff36fbd09cc34403b1f3ee
ReemaAlshahrani <reema.alshahrani.ali@gmail.com>
2026-09-19 10:34:05 +0000

    feat(ES6_basic): use default parameters to condense function internals to one line

--- ES6_basic/3-default-parameter.js
+++ ES6_basic/3-default-parameter.js
@@ -1,4 +1,3 @@
 export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
-  // Return the sum of the initial number and expansions using default parameter values
   return initialNumber + expansion1989 + expansion2019;
 }
