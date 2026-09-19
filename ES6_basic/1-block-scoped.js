e5dcd6fca8f13e01ec7b4cdf8c11776e0db8e7bc
ReemaAlshahrani <reema.alshahrani.ali@gmail.com>
2026-09-19 10:14:20 +0000

    feat(ES6_basic): correct block scope implementation with const and let

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,12 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-  // Use let for variables that will change their values
   let task = false;
   let task2 = true;
 
   if (trueOrFalse) {
-    // Reassign the outer variables directly without redeclaring them
-    task = true;
-    task2 = false;
+    const task = true;
+    const task2 = false;
+    return [task, task2];
   }
 
   return [task, task2];
