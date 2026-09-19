9d81cb092cf3245c0a04f6bdeea7251ac72a8216
ReemaAlshahrani <reema.alshahrani.ali@gmail.com>
2026-09-19 11:15:35 +0000

    feat(ES6 classes): Implement ClassRoom class to initialize a classroom with a maximum student size

---
+++ ES6_classes/0-classroom.js
@@ -0,0 +1,6 @@
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+    // Assign the maximum number of students to the classroom
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
