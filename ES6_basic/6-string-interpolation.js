692dfbc0ca99dc7b4ad41655070c07a151c558a3
ReemaAlshahrani <reema.alshahrani.ali@gmail.com>
2026-09-19 10:36:35 +0000

    feat(ES6_basic): use template literals for string interpolation

---
+++ ES6_basic/6-string-interpolation.js
@@ -0,0 +1,13 @@
+import getSanFranciscoDescription from './7-get_budget_object.js';
+
+export default function getSanFranciscoDescription() {
+  const year = 2017;
+  const budget = {
+    income: '$119,868',
+    gdp: '$154.2 billion',
+    capita: '$178,479',
+  };
+
+  // Use template literals to interpolate variables and object properties smoothly
+  return `As of ${year}, it was the seventh-highest income county in the United States, with a per capita personal income of ${budget.income}. As of 2015, San Francisco proper had a GDP of ${budget.gdp}, and a GDP per capita of ${budget.capita}.`;
+}
