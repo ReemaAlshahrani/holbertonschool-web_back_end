403e02b74ad66309cae2dac4941acd8c5b1969da
ReemaAlshahrani <reema.alshahrani.ali@gmail.com>
2026-09-19 10:27:40 +0000

    feat(ES6_basic): implement block scope correctly with comments

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,11 +1,14 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  // Define the outer variables with const
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
+    // These variables are block-scoped and only exist inside this if statement
     const task = true;
     const task2 = false;
   }
 
+  // Return the outer variables to ensure the correct output for both test cases
   return [task, task2];
 }
