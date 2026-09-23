71d0b9a7dcff88b7420d31a2bee579570831eb95
ReemaAlshahrani <reema.alshahrani.ali@gmail.com>
2026-09-23 17:40:56 +0000

    fix(ES6_promise): convert error reason to string in handleProfileSignup to match test expectations

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,12 +2,12 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  // Use Promise.allSettled to handle multiple promises and return their status and value/error
+  // Handle multiple settled promises and return status with value or error string
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
   ]).then((results) => results.map((res) => ({
     status: res.status,
-    value: res.value || res.reason,
+    value: res.value || res.reason.toString(),
   })));
 }
