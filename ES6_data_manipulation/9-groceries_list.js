32f123cda2acfc24b84f08aa088b62b4d7bfdfc0
ReemaAlshahrani <reema.alshahrani.ali@gmail.com>
2026-09-23 17:19:48 +0000

    feat(ES6_data_manipulation): create groceriesList function returning a Map of grocery items

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,12 @@
+/**
+ * Returns a Map data structure containing a list of groceries and their quantities.
+ */
+export default function groceriesList() {
+  return new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
+}
